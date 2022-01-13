66246551125293fe225f001022cf4daf0fad6f2b
murat201082 <murat201082@gmail.com>
2022-01-14 00:07:47 +0300

    14.01.2022 html and css works and voltran added

---
+++ fullstackkk lessons/style.js
@@ -0,0 +1,15 @@
+
+        //alert("Bugün Nasılsınız?");
+        document.writeln("Güzel Günleriniz Olsun");
+        console.log("Murat'ın Web Sitesi");
+        document.getElementById("doc").innerHTML="Web sitesine giriş yaptınız";
+        document.getElementById("docs").innerHTML="Bu alana İçerik Girilmez";
+        document.write("murats <br/>");
+        debugger;
+        document.write("murats <br/>");
+        document.write("murats <br/>");
+        document.write("murats <br/>");
+        document.write("murats <br/>");
+        
+      
+        
